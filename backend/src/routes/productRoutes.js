f69f036122e98590fab7eab9ad457b470a671c7a
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-31 15:44:21 +0530

    feat:added getproductbyid in routes

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { createProduct, getAllProducts } from '../controllers/productController.js';
 import { upload } from '../config/cloudinary.js';
+import { getProductById } from '../controllers/productController.js';
 
 const router = express.Router();
 
 // 'images' matches the name we will use in the Frontend form
 router.post('/add', upload.single('image'), createProduct);
 router.get('/all', getAllProducts);
+router.get('/:id', getProductById);
 
 export default router;
