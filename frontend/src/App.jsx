9eb2531beec39208d8669e39c5e0652ecf6deaf6
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-31 14:07:58 +0530

    feat: configure main routing for Home and Sell pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Import our custom components and pages
+import Navbar from './components/Navbar.js'
+import Home from './pages/Home.js'
+import SellProduct from './pages/SellProduct.js'
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* For now, just a simple heading to see if it works */}
-        <h1 style={{ textAlign: 'center', color: '#FF4D4D' }}>
-          Welcome to Ramtek Bazar
-        </h1>
+      <div className="min-h-screen bg-gray-50">
         
-        <Routes>
-          {/* We will add our Home and Sell routes here in the next step */}
-        </Routes>
+        {/* Navbar is outside <Routes> so it stays at the top of every page */}
+        <Navbar />
+
+        {/* This area changes based on the URL path */}
+        <main>
+          <Routes>
+            {/* http://localhost:3000/ */}
+            <Route path="/" element={<Home />} />
+
+            {/* http://localhost:3000/sell */}
+            <Route path="/sell" element={<SellProduct />} />
+          </Routes>
+        </main>
+
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
