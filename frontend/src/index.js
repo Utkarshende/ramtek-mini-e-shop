41b3c6aaa78f24373cc7b2d56275d06026b3c302
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-01-31 14:08:45 +0530

    chore: connect react to dom and import global styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css'; // This loads our Tailwind directives
 
+// Connecting React to the 'root' div in public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
