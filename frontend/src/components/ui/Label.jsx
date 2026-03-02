67efbe285e8a672542dd9c4ee7aff704f744e9f7
Utkarshende <139004110+Utkarshende@users.noreply.github.com>
2026-03-02 20:05:50 +0530

    chore:remove label component, integrated into inputfield

--- frontend/src/components/ui/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Label({text,htmlFor}){
-    return(
-        <label htmlFor={htmlFor} className="block text-sm font-semibold text-slate-300 mb-2">
-            {text}
-        </label>
-    );
-}
